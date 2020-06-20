5c916ba8d4f8377444bdf0a3e5a9bebd801a954e
Sam Cross <samgcdev+git@gmail.com>
2020-06-20 15:22:19 +0100

    style: PosterEdit styles

--- elyreacts/src/pages/PosterEdit.js
+++ elyreacts/src/pages/PosterEdit.js
@@ -1,123 +1,100 @@
-import {GlobalContext} from "../context/GlobalState";
-import React, {useEffect, useContext, useState} from "react";
+import { GlobalContext } from "../context/GlobalState";
+import React, { useEffect, useContext, useState } from "react";
+import { FormInput } from "../components/FormInput";
+
 // @ts-ignore
-import cn from './styles/Poster.module.scss';
-import {FormInput} from "../components/FormInput";
+import cn from "./styles/PosterEdit.module.scss";
 
-export const PosterEdit = ({
-    posterID
-}) => {
-    let {
-        user,
-        log_status,
-        getPoster,
-        poster,
-        editPoster,
-        deletePoster
-    } = useContext(GlobalContext);
+const getPictureUrl = (pictureUrl) => {
+    try {
+        return require("../assets/postersDb/" + pictureUrl.split("Db")[1].substring(1));
+    } catch (err) {
+        // todo/fixme: Remove this as it shouldn't be necessary outside of testing
+        return "https://source.unsplash.com/random";
+    }
+};
 
-    console.log(posterID);
+export const PosterEdit = ({ posterID }) => {
+    let { getPoster, poster, editPoster, deletePoster } = useContext(GlobalContext);
+
     useEffect(() => {
-        
         getPoster(posterID);
     }, []);
 
-    let picUrl = null;
-    try {
-        let purl = poster.pictureURL.split('Db\\')[1];
-        picUrl = require("../assets/postersDb/" + purl);
-    } catch{let x = 0;}
-
     poster = {
         ...poster,
-        id: poster && poster._id || posterID,
-        title: poster && poster.title || 'Untitled',
-        author: poster && poster.madeBy || 'Unknown',
-        category: poster && poster.category || 'Unknown',
-        caption: poster && poster.caption|| "Caption",
-        price: poster && poster.price || 0.0,
-        views: poster && poster.views || 0,
-        admires: poster && poster.admires || 0,
-        tags: poster && poster.tags || 'Unknown'
+        id: (poster && poster._id) || posterID,
+        title: (poster && poster.title) || "Untitled",
+        author: (poster && poster.madeBy) || "Unknown",
+        category: (poster && poster.category) || "Unknown",
+        caption: (poster && poster.caption) || "Caption",
+        price: (poster && poster.price) || 0.0,
+        views: (poster && poster.views) || 0,
+        admires: (poster && poster.admires) || 0,
+        tags: (poster && poster.tags) || "Unknown",
     };
 
-    console.log(poster.title);
-    let ti = poster.title
-    console.log(ti)
-    const [title, setTitle] = useState(ti);
-    const [caption, setCaption] = useState(poster.caption) 
+    const [title, setTitle] = useState(poster.title);
+    const [caption, setCaption] = useState(poster.caption);
     const [tags, setTags] = useState(poster.tags);
-    const handleFormSubmit = (e, title, caption, tags) => {
-        let edittedForm = {
-            title: title,
-            caption: caption,
-            tags: tags
-        };
+
+    const handleFormSubmit = (e, formTitle, formCaption, formTags) => {
         e.preventDefault();
-        setTitle(title);
-        setCaption(caption);
-        editPoster(poster.id, edittedForm);
+
+        setTitle(formTitle);
+        setCaption(formCaption);
+
+        editPoster(poster.id, {
+            formTitle,
+            formCaption,
+            formTags,
+        });
     };
 
     const handleDeletePoster = (e) => {
         e.preventDefault();
-        console.log("deletign ")
         deletePoster(posterID);
-    }
+    };
 
     return (
-        <div className="page-container">
-            <div className="page-header">
-                <h1>{title}</h1>
-                <p>{caption}</p>
+        <div className='page-container'>
+            <div className='page-header'>
+                <h1>Edit Poster</h1>
+                <p>Editing poster: {title}</p>
             </div>
 
             <div className={`${cn.contentContainer} lower-content-container`}>
                 <div className={cn.imageContainer}>
-                    {picUrl !== null ? <img src={picUrl} alt={poster.title} />
-                        : <img src={picUrl} alt={poster.title} />
-                    }
-
+                    <img src={getPictureUrl(poster.pictureURL)} alt={poster.title} />
                 </div>
 
-                <div className={`${cn.informationContainer}`}>
-
-                    <small>{`By ${poster.author}`}</small>
-                    <div className={cn.admiresContainer}>
-                        <span>❤</span>
-                        <strong>{poster.admires}</strong>
-                    </div>
-
-                    <div className={cn.viewsContainer}>
-                        <span>👁️</span>
-                        <strong>{poster.views}</strong>
-                    </div>
-
-                    <form onSubmit={e => handleFormSubmit(e, title, caption, tags)}>
+                <div className='form-container'>
+                    <form onSubmit={(e) => handleFormSubmit(e, title, caption, tags)}>
                         <FormInput
-                            type="text"
-                            name="Title"
+                            type='text'
+                            name='Title'
                             value={title}
-                            onChange={(e) => setTitle(e.target.value)} />
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
                         <FormInput
-                            type="text"
-                            name="Caption"
+                            type='text'
+                            name='Caption'
                             value={caption}
-                            onChange={(e) => setCaption(e.target.value)} />
+                            onChange={(e) => setCaption(e.target.value)}
+                        />
                         <FormInput
-                            type="text"
-                            name="tags"
+                            type='text'
+                            name='tags'
                             value={tags}
-                            onChange={(e) => setTags(e.target.value)} />
-                        <button>Submit edits</button>
+                            onChange={(e) => setTags(e.target.value)}
+                        />
+                        <button type='submit'>Submit</button>
+                        <button className={cn.buttonDelete} onClick={(e) => handleDeletePoster(e)}>
+                            Delete Poster
+                        </button>
                     </form>
-                    <button onClick={(e)=>handleDeletePoster(e)}>Delete poster</button>
-                    <h6>If you delete the poster you will not be able to
-                        make any profits from it.</h6>
-                    <strong className={cn.price}>{poster.price.toFixed(2)}</strong>
-                    <h4>You can not edit the poster price.</h4>
-                    <div className={`${cn.formContainer} form-container`}>
-                    </div>
+
+                    <small>Deleting a poster is irreversible.</small>
                 </div>
             </div>
         </div>
