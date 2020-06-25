b189e5e32103bcfa756e06192007080babb59a39
Sam Cross <samgcdev+git@gmail.com>
2020-06-25 13:21:06 +0100

    fix: update import

--- elyreacts/src/pages/OrdersList.js
+++ elyreacts/src/pages/OrdersList.js
@@ -1,22 +1,17 @@
-import React, { useContext, useEffect } from 'react'
-import { GlobalContext } from '../context/GlobalState'
-import { OrderItem } from './OrderItem'
+import React, { useContext, useEffect } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { OrderItem } from "../components/OrderItem";
 
 export const OrdersList = () => {
-
-    const {orders,getOrders} = useContext(GlobalContext)
+    const { orders, getOrders } = useContext(GlobalContext);
 
     useEffect(() => {
-        getOrders()
-    }, [])
+        getOrders();
+    }, []);
     return (
         <div>
             <h1>Your Orders - {orders.length}</h1>
-            {orders&&orders.length>0?
-                orders.map((oi)=>
-                <OrderItem order={oi}/>
-            ):null
-            }
+            {orders && orders.length > 0 ? orders.map((oi) => <OrderItem order={oi} />) : null}
         </div>
-    )
-}
+    );
+};
