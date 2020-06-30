eb6b1a3caf257c3e3d741fd5ad83d3588ebe7e09
Sam Cross <samgcdev@gmail.com>
2020-06-30 11:20:05 +0100

    feat: Add content to footer

--- elyreacts/src/components/Footer.js
+++ elyreacts/src/components/Footer.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 // @ts-ignore
-import cn from './styles/Footer.module.scss';
+import cn from "./styles/Footer.module.scss";
 
 export default () => {
     return (
         <footer className={cn.container}>
             <div className={cn.content}>
                 <span>Elywalls</span>
+
+                <ul className='regular'>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/posters'>Posters</Link>
+                    </li>
+                    <li>
+                        <Link to='/orders'>Your Orders</Link>
+                    </li>
+                    <li>
+                        <Link to='/account'>Your Account</Link>
+                    </li>
+                    <li>
+                        <a href='mailto:elywalls@gmail.com'>Email Us</a>
+                    </li>
+                </ul>
+
+                <span className={cn.copyright}>&copy; 2020 - Elywalls</span>
+                <span className={cn.copying}>Copying user-uploaded content is prohibited</span>
             </div>
         </footer>
     );
