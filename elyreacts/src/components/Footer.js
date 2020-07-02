5a99fbfb5a5d0c62e775e76b08753797269c7cbe
Sam Cross <samgcdev@gmail.com>
2020-07-02 16:01:34 +0100

    feat: new Footer

--- elyreacts/src/components/Footer.js
+++ elyreacts/src/components/Footer.js
@@ -1,35 +1,79 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+//@ts-ignore
+import ELYWALLS_LOGO from "../assets/images/Logo.svg";
+
 // @ts-ignore
 import cn from "./styles/Footer.module.scss";
 
 export default () => {
     return (
         <footer className={cn.container}>
-            <div className={cn.content}>
-                <span>Elywalls</span>
+            <div className={cn.mainContent}>
+                <div className={cn.logoContainer}>
+                    <img src={ELYWALLS_LOGO} alt='Elywalls' />
+                </div>
 
-                <ul className='regular'>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/posters'>Posters</Link>
-                    </li>
-                    <li>
-                        <Link to='/orders'>Your Orders</Link>
-                    </li>
-                    <li>
-                        <Link to='/account'>Your Account</Link>
-                    </li>
-                    <li>
-                        <a href='mailto:elywalls@gmail.com'>Email Us</a>
-                    </li>
-                </ul>
+                <nav>
+                    <h1>Popular Pages</h1>
 
+                    <ul className='regular no-bullets'>
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/posters'>Posters</Link>
+                        </li>
+                        <li>
+                            <Link to='/orders'>Your Orders</Link>
+                        </li>
+                        <li>
+                            <Link to='/account'>Your Account</Link>
+                        </li>
+                        <li>
+                            <a href='mailto:elywalls@gmail.com'>Email Us</a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div>
+                    <h1>Categories</h1>
+
+                    <ul className='regular no-bullets'>
+                        <li>
+                            <Link to='/posters/all'>All Posters</Link>
+                        </li>
+                        <li>
+                            <Link to='/posters/latest'>Latest</Link>
+                        </li>
+                        <li>
+                            <Link to='/posters/photography'>Photography</Link>
+                        </li>
+                        <li>
+                            <Link to='/posters/textography'>Textography</Link>
+                        </li>
+                        <li>
+                            <Link to='/posters/graphic-design'>Graphic Design</Link>
+                        </li>
+                        <li>
+                            <Link to='/posters/photoshop'>Photoshop</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className={cn.lowerContent}>
                 <span className={cn.copyright}>&copy; 2020 - Elywalls</span>
-                <span className={cn.copying}>Copying user-uploaded content is prohibited</span>
+                <small>
+                    Designed in the UK by{" "}
+                    <a rel='noreferrer noopener' target='_blank' href='https://samcross.digital/'>
+                        Sam Cross Digital
+                    </a>
+                </small>
+                <small className={cn.copying}>
+                    Unauthorised duplication of user-uploaded content is prohibited by law
+                </small>
             </div>
         </footer>
     );
