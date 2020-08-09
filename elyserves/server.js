0ccb67035ad67af8c21edd7a56ebac93797ac6b9
saaketh <saakethlogs@gmail.com>
2020-08-09 17:23:03 +0530

    Version 1.0 :D

--- elyserves/server.js
+++ elyserves/server.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 require('colors');
 dotenv.config({path: "config.env"});
 const app = express();
@@ -12,6 +14,17 @@ const poster_routes = require("./routes/posterRoutes");
 const user_routes = require("./routes/userRoutes");
 app.use("/", poster_routes);
 app.use("/", user_routes);
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('../elyreacts/build'))
+    app.get('*',(req,res)=> res.sendFile(path.resolve(
+      __dirname,'../elyreacts','build','index.html'
+    )))
+  }
+  
+  console.log(path.resolve(
+    __dirname,'../elyreacts','build','index.html'
+  ))
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -25,10 +38,10 @@ mongoose.connect(process.env.MONGO_URI, {
     }
 });
 
-app.listen(5000, (err) => {
+app.listen(process.env.SERVER_PORT, (err) => {
     if (err) {
         console.log("Server not listening: \n".red.bold, err);
     } else {
-        console.log(`Server listening at port ${process.env.PORT}`.blue.bold);
+        console.log(`Server listening at port ${process.env.SERVER_PORT}`.blue.bold);
     }
 });
